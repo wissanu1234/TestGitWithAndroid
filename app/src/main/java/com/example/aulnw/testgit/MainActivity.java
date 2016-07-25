9ddd7e2cf410158137b6f3ea15fd859472bb3a48
wissanu1234 <auau_thai@hotmail.com>
2016-07-25 19:38:23 +0700

    Add flagment_main + MainFragment  + add MainFragment in MainActivity

--- app/src/main/java/com/example/aulnw/testgit/MainActivity.java
+++ app/src/main/java/com/example/aulnw/testgit/MainActivity.java
@@ -3,11 +3,25 @@ package com.example.aulnw.testgit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.aulnw.testgit.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInstances();
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentContainer, MainFragment.newInstance())
+                    .commit();
+        }
+    }
+
+    private void initInstances() {
+
     }
 }
